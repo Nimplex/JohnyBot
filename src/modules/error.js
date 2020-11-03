dc13aabaf314f4e5c2634e63edafd2197b4414c0
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-04 00:26:38 +0100

    less useless logs.

--- src/modules/error.js
+++ src/modules/error.js
@@ -1,11 +1,11 @@
 const { red } = require('chalk')
 
-module.exports = (messageObject, errorMessage) => {
+module.exports = (messageObject, errorMessage, log = false) => {
   client.modules.embed({
     title: 'Error',
     color: client.config.rcolor,
     description: `\`\`\`An unexpected error has occurred. Error message: ${errorMessage}\`\`\``,
     message: messageObject
   })
-  console.error(`Error: ${red(errorMessage)}`)
+  if(log) console.error(`Error: ${red(errorMessage)}`)
 }
