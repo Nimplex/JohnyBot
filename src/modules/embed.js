e457e632c8a43aefa73b8a5d9d1434fb883e7b6c
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-03 23:36:53 +0100

    improved embed module + ban command.

--- src/modules/embed.js
+++ src/modules/embed.js
@@ -3,22 +3,23 @@ const { MessageEmbed } = require('discord.js')
 module.exports = data => {
   const embed  = new MessageEmbed()
   const config = Object.assign({ 
-    title: '', 
+    title: undefined, 
     color:  client.config.color, 
-    description: '',
-    thumbnail: '',
-    image: '',
-    author: '',
-    fields: [],
+    description: undefined,
+    thumbnail: undefined,
+    image: undefined,
+    author: undefined,
+    fields: undefined,
     footer: `Invoked by ${data.message.author.tag}`,
     timestamp: new Date(),
-    message: ''
+    message: undefined
   }, data)
   if (config.title)       embed.setTitle(config.title)
   if (config.color)       embed.setColor(config.color)
   if (config.description) embed.setDescription(config.description)
   if (config.thumbnail)   embed.setThumbnail(config.thumbnail)
   if (config.image)       embed.setImage(config.image)
+  if (config.author)      embed.setAuthor(config.author[0], config.author[1])
   if (config.footer)      embed.setFooter(config.footer)
   if (config.timestamp)   embed.setTimestamp(config.timestamp)
   if (config.fields)      embed.addFields(config.fields)
