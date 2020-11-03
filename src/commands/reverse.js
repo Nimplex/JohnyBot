4d6f72251a474f2617683f17e4cca0a9877f3b74
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-03 23:53:11 +0100

    8ball + reverse

---
+++ src/commands/reverse.js
@@ -0,0 +1,20 @@
+exports.execute = (client, message, args) => {
+  if (!args[0]) return client.modules.error(message, 'Please provide valid arguments.')
+  const text = args.join(' ').split('').reverse().join('')
+  client.modules.embed({
+    message: message,
+    title: `:arrows_counterclockwise:`,
+    description: `\`\`\`${text}\`\`\``
+  })
+}
+
+exports.info = {
+  triggers: ['reverse'],
+  description: 'Reverse text.',
+  usage: '<text to reverse>',
+  permissions: {
+    bot: [],
+    member: [],
+    developer: false
+  }
+}
