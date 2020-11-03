38dce4e1f8c7d509345c55cd4cf325c6385c0e92
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-03 22:49:23 +0100

    better permissions.

--- src/handlers/message.js
+++ src/handlers/message.js
@@ -9,6 +9,7 @@ module.exports = message => {
     const cmd = client.commands.get(command)
     if (cmd.info.permissions.developer && message.author.id !== '364056796932997121') return client.modules.error(message, 'You need to have developer permissions to use this command.')
     else if (!message.member.permissions.has(cmd.info.permissions.member)) return client.modules.error(message, `You need to have ${cmd.info.permissions.member} permission(s) to use this command.`)
+    else if (!message.guild.me.permissions.has(cmd.info.permissions.bot)) return client.modules.error(message, `I'm missing permission(s): ${cmd.info.permissions.member}.`)
     else cmd.execute(client, message, args)
   } catch(err) {
     client.modules.error(message, err)
