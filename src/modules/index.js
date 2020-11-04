897b6cb03b9c117315152f757cbc8b6ec3408429
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-04 23:50:14 +0100

    cleanup

--- src/modules/index.js
+++
@@ -1,9 +0,0 @@
-// Thanks for badosz0 [https://github.com/badosz0/badosz-bot]
-
-require('fs')
-  .readdirSync(__dirname)
-  .filter(file => file !== 'index.js' && file.includes('.'))
-  .map(filename => {
-    const moduleName = filename.split('.')[0]
-    exports[moduleName] = require(`${__dirname}/${filename}`)
-})
