082fad0454386b333bd1f5e9cdb2f3f0f505157d
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-10 14:28:00 +0100

    easter egg

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
 module.exports = () => {
   require('../handlers/plugins.js')()
-  Johny.console.log(`${Johny.user.tag} is ready.`)
+  Johny.console.log(`${Johny.user.tag} ${Math.floor(Math.random() * 100) == 20 ? 'will Rock now': 'is ready'}.`)
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ class Johny extends Client {
     super(conf)
 
     global.Johny      = this
+    this.commands     = 0
     this.files        = ['tokens', 'prefixes', 'colors']; this.files.forEach(file => this[file] = require(`./files/${file}`))
     this.prefix       = this.prefixes.prefix
     this.http         = require('node-fetch')
