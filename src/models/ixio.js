b407d949a644abc8cdf625f9c654e8c5591a26da
Nimplex <39964594+Nimplex@users.noreply.github.com>
2020-11-27 20:03:56 +0100

    Update ixio.js

--- src/models/ixio.js
+++ src/models/ixio.js
@@ -1,8 +1,6 @@
-const base_url = 'http://ix.io'
-
 module.exports = async data => {
   let key = ''
   const params = new URLSearchParams(); params.append('f:1', data)
-  await Johny.http(`${base_url}`, { method: 'post', body: params }).then(res => res.text()).then(text => key = text.replace('\n', ''))
+  await Johny.http(`http://ix.io`, { method: 'post', body: params }).then(res => res.text()).then(text => key = text.replace('\n', ''))
   return [`${key}`, `${key}/`]
 }
